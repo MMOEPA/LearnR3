d388aee340ebff41ba18888cc8eca4e575f8213f
Maria Elizabeth Enge Moeller <maria.elizabeth.engel.moeller.02@regionh.dk>
2023-04-26 09:04:17 +0200

    Add GitHub links to DESCRIPTION

---
+++ R/learning.R
@@ -0,0 +1,15 @@
+# Basics of R -------------------------------------------------------------
+
+colnames(airquality)
+str(airquality)
+Summary(airquality)
+
+mean(2:6)
+
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
+r3::check_git_config()
+
+#here is an example of a conflict
